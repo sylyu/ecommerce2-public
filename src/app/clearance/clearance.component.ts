946e389e68823cf83b092fc2f698eec0ef492501
Sylvia <sylviah.yu@gmail.com>
2019-03-14 14:51:36 -0700

    Worked on Clearance page - formatted bootstrap html with css - wrote backend code in ts file

--- src/app/clearance/clearance.component.ts
+++ src/app/clearance/clearance.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from "../http.service";
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-clearance',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./clearance.component.css']
 })
 export class ClearanceComponent implements OnInit {
+  products: any;
 
-  constructor() { }
+  constructor(
+    private _httpService: HttpService,
+    private _route: ActivatedRoute,
+    private _router: Router,
+  ) {}
 
   ngOnInit() {
+    this.products = [];
+    this.getProducts();
+  }
+  // currency format for price of product
+  getPrice(floatNumber) {
+    if (floatNumber) {
+      return floatNumber.toLocaleString('us-US', { style: 'currency', currency: 'USD' });
+    } else {
+      return "--";
+    }
+  }
+
+  price(product) {
+    return this.getPrice(product.price);
+  }
+
+  getProducts() {
+    let observable = this._httpService.getProducts();
+    observable.subscribe(data => {
+      this.products = data['products'];
+      console.log(this.products);
+    });
+  }
+
+  deleteProduct(id) {
+    console.log("delete product with id", id);
+    let observable = this._httpService.deleteProduct(id);
+    observable.subscribe(data => {
+      console.log(data);
+      this.getProducts();
+    });
   }
 
 }
